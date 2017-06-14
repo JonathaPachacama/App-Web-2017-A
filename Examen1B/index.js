7cdd937aada09851d90d0b33c256b1c3c6480db5
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-06-14 15:29:19 -0500

    rutas facultad cabecera

--- Examen1B/index.js
+++ Examen1B/index.js
@@ -3,13 +3,29 @@
  */
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-})
+const fs = require('fs');
+app.use(express.static('public'));
+//Ruta Informacion
 app.post('/informacion', function (req, res) {
-    res.send('Got a POST request')
+    res.append('insertar','valor')
+    fs.readFile('Informacion.txt', 'utf8',
+        function(err, data) {
+            if (err) throw err;
+            console.log(data);
+            res.send(data);
+        });
+})
+//Ruta Facultad de Sistemas
+app.get('/facultaddesistemas', function (req, res) {
+})
+
+//Ruta Cabeceras
+app.post('/cabeceras', function (req, res) {
+    res.append('nombre-cabecera','Valor de la cabecera');
 })
 app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
+
+
+
