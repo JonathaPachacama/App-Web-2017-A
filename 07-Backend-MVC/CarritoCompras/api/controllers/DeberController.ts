5f4adbf7222e3effb9ce6e7c658836246971e85c
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-06-25 22:09:37 -0500

    res.forbidden()

--- 07-Backend-MVC/CarritoCompras/api/controllers/DeberController.ts
+++ 07-Backend-MVC/CarritoCompras/api/controllers/DeberController.ts
@@ -37,7 +37,17 @@ module.exports = {
   },
   Ejemplo3:(req,res)=>{
     //res.created()
+    // Este método se utiliza para enviar un 201 respuesta ( "Creado") de vuelta al cliente que indica que uno o más recursos se han creado con éxito.
     return res.created('Nuevo widget creado.');
   },
+  Ejemplo4:(req,res)=>{
+    // res.forbidden ()
+    //Este método se utiliza para enviar un 403 respuesta ( "Prohibido") de vuelta al cliente indicando que la solicitud no está permitido.
+    if ( !req.session.canEditSalesforceLeads ) {
+      return res.forbidden('Se requiere acceso de escritura');
+    }
+  },
+
+
 };
 
