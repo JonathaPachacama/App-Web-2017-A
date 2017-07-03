8a45317e665efc0c26be40b46a72393934ab3d56
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-07-03 12:55:47 -0500

    usuario

--- 07-Backend-MVC/CarritoCompras/api/models/Usuario.ts
+++ 07-Backend-MVC/CarritoCompras/api/models/Usuario.ts
@@ -4,6 +4,22 @@
 declare var module:any;
 
 module.export={
-
+  attributes:{
+    nombres:{
+      type:"string"
+    },
+    apellidos:{
+      type:"string"
+    },
+    password:{
+      type:"string"
+    },
+    correo:{
+      type:"email"
+    },
+    fechaNacimiento:{
+      type:"date"
+    }
+  }
 
 };
