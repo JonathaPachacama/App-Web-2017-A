06cdd65dd310617bc565c464d3b155fca7082676
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-07-08 08:12:28 -0500

    nuta homepage y EJS

--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
@@ -1,5 +1,15 @@
 module.exports = {
     vistaOculta: function (req, res) {
         return res.view('Oculto/sorpresa');
+    },
+    homepage: function (req, res) {
+        var usuarioModelo = {
+            nombres: "Jonathan",
+            apellidos: "Pachacama",
+            id: "1",
+        };
+        return res.view('homepage', {
+            usuario: usuarioModelo
+        });
     }
 };
--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
@@ -2,6 +2,18 @@ declare var module;
 module.exports = {
   vistaOculta:(req,res)=>{
     return res.view('Oculto/sorpresa')
+  },
+
+  homepage:(req,res)=>{
+    let usuarioModelo ={
+      nombres: "Jonathan",
+      apellidos:"Pachacama",
+      id:"1",
+    };
+
+    return res.view('homepage',{
+      usuario:usuarioModelo
+    })
   }
 
 };
