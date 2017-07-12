a57bf0ab174dce0cdb9d3f282a7489c6b27d34c8
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-07-12 09:19:24 -0500

    taller usuarios homepage

--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.js
@@ -3,13 +3,28 @@ module.exports = {
         return res.view('Oculto/sorpresa');
     },
     homepage: function (req, res) {
-        var usuarioModelo = {
-            nombres: "Jonathan",
-            apellidos: "Pachacama",
-            id: "1",
-        };
-        return res.view('homepage', {
-            usuario: usuarioModelo
+        // let usuarioModelo ={
+        //   nombres: 'joanathan',
+        //   apellidos:"Pachacama",
+        //   id:"1",
+        // };
+        Usuario.find().exec(function (err, usuariosEncontrados) {
+            if (err)
+                return res.negotiate(err);
+            return res.view('homepage', {
+                usuarios: usuariosEncontrados
+            });
         });
+        ////Otra forma
+        // homepage: (req,res) => {
+        //   sails.models.usuario.find().exec(
+        //     (err, usuariosEncontrados) => {
+        //       if (err) return res.serverError(err)
+        //       return res.view('homepage',{usuarios:usuariosEncontrados})
+        //     }
+        //   )}
+        // return res.view('homepage',{
+        //   usuario:usuarioModelo
+        // })
     }
 };
--- 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
+++ 07-Backend-MVC/CarritoCompras/api/controllers/VistaController.ts
@@ -5,15 +5,32 @@ module.exports = {
   },
 
   homepage:(req,res)=>{
-    let usuarioModelo ={
-      nombres: "Jonathan",
-      apellidos:"Pachacama",
-      id:"1",
-    };
+    // let usuarioModelo ={
+    //   nombres: 'joanathan',
+    //   apellidos:"Pachacama",
+    //   id:"1",
+    // };
 
-    return res.view('homepage',{
-      usuario:usuarioModelo
-    })
+    Usuario.find().exec((err, usuariosEncontrados)=>{
+      if (err)return res.negotiate(err);
+      return res.view('homepage',{
+        usuarios:usuariosEncontrados
+      })
+
+    });
+    ////Otra forma
+    // homepage: (req,res) => {
+    //   sails.models.usuario.find().exec(
+    //     (err, usuariosEncontrados) => {
+    //       if (err) return res.serverError(err)
+    //       return res.view('homepage',{usuarios:usuariosEncontrados})
+    //     }
+    //   )}
+
+
+    // return res.view('homepage',{
+    //   usuario:usuarioModelo
+    // })
   }
 
 };
